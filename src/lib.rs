b395072c74536f9db5b5bd4de2a7d532389e725f
letung3105 <15053897+letung3105@users.noreply.github.com>
2023-02-05 13:39:38 +0700

    add initialization function

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,22 @@ use core::{arch::asm, panic::PanicInfo};
 
 use uart_ns16550::{UartDriver, QEMU_VIRT_UART_MMIO_ADDRESS};
 
+extern "C" {
+    static TEXT_START: usize;
+    static TEXT_END: usize;
+    static RODATA_START: usize;
+    static RODATA_END: usize;
+    static DATA_START: usize;
+    static DATA_END: usize;
+    static BSS_START: usize;
+    static BSS_END: usize;
+    static KERNEL_STACK_START: usize;
+    static KERNEL_STACK_END: usize;
+    static HEAP_START: usize;
+    static HEAP_SIZE: usize;
+    static mut KERNEL_TABLE: usize;
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo<'_>) -> ! {
     print!("Aborting: ");
@@ -47,13 +63,24 @@ extern "C" fn abort() -> ! {
 }
 
 #[no_mangle]
-extern "C" fn kmain() {
-    let mut uart = unsafe {
+extern "C" fn kinit() -> usize {
+    unsafe {
         UartDriver::initialize_global(QEMU_VIRT_UART_MMIO_ADDRESS);
-        UartDriver::new(QEMU_VIRT_UART_MMIO_ADDRESS)
-    };
-
-    println!("Hello world!");
+        println!("TEXT_START   = {}", TEXT_START);
+        println!("TEXT_END     = {}", TEXT_END);
+        println!("DATA_START   = {}", DATA_START);
+        println!("DATA_END     = {}", DATA_END);
+        println!("RODATA_START = {}", RODATA_START);
+        println!("RODATA_END   = {}", RODATA_END);
+        println!("BSS_START    = {}", BSS_START);
+        println!("BSS_END      = {}", BSS_END);
+        println!("KERNEL_STACK = {}", KERNEL_STACK_START);
+        println!("KERNEL_STACK = {}", KERNEL_STACK_END);
+        println!("HEAP_START   = {}", HEAP_START);
+        println!("HEAP_SIZE    = {}", HEAP_SIZE);
+        println!("KERNEL_TABLE = {}", KERNEL_TABLE);
+    }
+    let mut uart = unsafe { UartDriver::new(QEMU_VIRT_UART_MMIO_ADDRESS) };
     loop {
         let c = match uart.get() {
             None => continue,
@@ -92,4 +119,10 @@ extern "C" fn kmain() {
             _ => print!("{}", c as char),
         }
     }
+}
+
+#[no_mangle]
+extern "C" fn kmain() -> ! {
+    // TODO: separate initialization and long running tasks
+    loop {}
 }
